c6f2c4c93e36448259ec735130e896de238d2a3b
morph0z <131499168+morph0z@users.noreply.github.com>
2025-10-31 10:39:29 -0400

    kinda fully implemented dynamic music

--- scripts/ui/dynamic_music.gd
+++ scripts/ui/dynamic_music.gd
@@ -5,10 +5,31 @@ extends AudioStreamPlayer
 @export var tensenessLayers:Array[AudioStream]
 @export var overlapLayers:bool = false
 var playerRef:PlayerClass
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	stream = baseTrack
 	playing = true
 
-func _process(_delta: float) -> void:
-	print()
+var playOnce:bool = false
+var stopOnce:bool = false
+var musicLayer:AudioStreamPlayer 
+func _process(delta: float) -> void:
+	if get_parent() is generatingWorld:
+		playerRef = get_parent().get_node("Player")
+		musicLayer = AudioStreamPlayer.new()
+		get_parent().add_child(musicLayer)
+		musicLayer.bus = "Music"
+		if playerRef.health_component.HealthPoints > 50:
+			musicLayer.stream = tensenessLayers[0]
+			if !playOnce:
+				musicLayer.play()
+				playOnce = true
+		if playerRef.health_component.HealthPoints <= 50:
+			if !stopOnce:
+				musicLayer.stop()
+				playOnce = false
+				stopOnce = true
+			musicLayer.stream = tensenessLayers[1]
+			if !playOnce:
+				musicLayer.play(get_playback_position())
+				playOnce = true
